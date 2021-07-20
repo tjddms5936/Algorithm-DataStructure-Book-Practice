c08245fb35dbed0b69fe75e375e2366fbcecd485
tjddms5936 <tjddms5936@naver.com>
2021-07-20 14:03:25 +0900

    Add Q&A 6-7

--- 연습문제6-7.c
+++ 연습문제6-7.c
@@ -2,15 +2,38 @@
 #include <stdlib.h>
 
 // 단순 삽입 정렬 함수
+void print(const int b[], int all, int already, int still){
+    int i;
+    for(i=0; i<all; i++){
+        if(i == already){
+            printf("* ");
+        }
+        else if(i == still){
+            printf("+ ");
+        }
+        else{
+            printf(" ");
+        }
+    }
+    printf("\n");
+    for(i=0; i<all; i++){
+        printf("%d ", b[i]);
+    }
+    printf("\n");
+}
+
 void insertion(int a[], int n){
     int i, j;
+    int already;
     for(i=1; i<n; i++){
         int tmp = a[i];
         for(j = i; j>0 && a[j-1] > tmp; j--){
             a[j] = a[j-1];
+            already = j-1;
         }
         a[j] = tmp;
     }
+    print(a, n, already, i);
 }
 
 int main(void){
