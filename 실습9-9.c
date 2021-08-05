cef7bd505f2356134547c320f9a201b203588dff
tjddms5936 <tjddms5936@naver.com>
2021-08-05 22:05:05 +0900

    Add Practice 9-9

--- 실습9-9.c
+++ 실습9-9.c
@@ -10,7 +10,7 @@ static Dnode *AllocDNode(void){
 }
 
 // 노드의 각 멤버 값을 설정
-static void SetNode(Dnode *n, const Member *x, const Dnode *prev, const Dnode *next){
+static void SetNode(Dnode *n,  Member *x,  Dnode *prev, Dnode *next){
     n->data = *x; // 데이터
     n->prev = prev; // 앞쪽 노드에 대한 포인터
     n->next = next; // 다음 노드에 대한 포인터
@@ -190,4 +190,95 @@ void Clear(Dlist *list){
 void Terminate(Dlist *list){
     Clear(list); // 모든 노드를 삭제  하고나면 더미 노드만 남음
     free(list->head); // 더미 노드를 삭제 
+}
+
+
+// 메뉴 
+typedef enum{
+    TERMINATE, INS_FRONT, INS_REAR, RMV_FRONT, RMV_REAR, PRINT_CRNT, RMV_CRNT, SRCH_NO, SRCH_NAME, PRINT_ALL, NEXT, PREV, CLEAR
+}Menu;
+
+// 메뉴 선택
+Menu SelectMenu(void){
+    int i, ch;
+    char *mstring[] = {
+        "머리에 노드를 삽입", "꼬리에 노드를 삽입", "머리 노드를 삭제", "꼬리 노드를 삭제",
+        "선택한 노드를 출력", "선택한 노드를 삭제", "번호로 검색", "이름으로 검색", "모든 노드를 출력",
+        "선택한 노드를 뒤쪽으로", "선택한 노드를 앞쪽으로", "선택한 노드를 앞쪽으로", "모든 노드를 삭제"
+    };
+    do{
+        for(i = TERMINATE; i < CLEAR; i++){
+            printf("(%2d) %-22.22s ", i + 1, mstring[i]);
+            if((i % 3) == 2){
+                printf("\n");
+            }
+        }
+        printf("(0) 종료 : ");
+        scanf("%d", &ch);
+    }while(ch < TERMINATE || ch > CLEAR);
+    return(Menu)ch;
+}
+
+int main(void){
+    Menu menu;
+    Dlist list;
+    Initialize(&list); // 원형 이중 연결 리스트를 초기화
+    do{
+        Member x;
+        switch (menu = SelectMenu())
+        {
+        case INS_FRONT:
+            x = ScanMember("머리에 삽입", MEMBER_NO | MEMBER_NAME);
+            InsertFront(&list, &x);
+            break;
+        case INS_REAR:
+            x = ScanMember("꼬리에 삽입", MEMBER_NO | MEMBER_NAME);
+            InsertRear(&list, &x);
+            break;
+        case RMV_FRONT:
+            RemoveFront(&list);
+            break;
+        case RMV_REAR:
+            RemoveRear(&list);
+            break;
+        case PRINT_CRNT:
+            PrintLnCurrent(&list);
+            break;
+        case RMV_CRNT:
+            RemoveCurrent(&list);
+            break;
+        case SRCH_NO:
+            x = ScanMember("검색", MEMBER_NO);
+            if(search(&list, &x, MemberNoCmp) != NULL){
+                PrintLnCurrent(&list);
+            }
+            else{
+                printf("그 번호의 데이터가 없습니다.");
+            }
+            break;
+        case SRCH_NAME:
+            x = ScanMember("검색", MEMBER_NAME);
+            if(search(&list, &x, MemberNoCmp) != NULL){
+                PrintLnCurrent(&list);
+            }
+            else{
+                printf("그 이름의 데이터가 없습니다.");
+            }
+            break;
+        case PRINT_ALL:
+            Print(&list);
+            break;
+        case NEXT:
+            Next(&list);
+            break;
+        case PREV:
+            Prev(&list);
+            break;
+        case CLEAR:
+            Clear(&list);
+            break;
+        }
+    }while(menu != TERMINATE);
+    Terminate(&list);
+    return 0;
 }
